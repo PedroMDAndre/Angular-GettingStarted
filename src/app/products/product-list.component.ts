a0e6ef6a11aab9052391280b8f7894f83b8eabdc
PedroMDAndre <pedro.mda@gmail.com>
2021-04-28 16:55:56 +0100

    services and dependencies injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { Iproduct } from "./product";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: "pm-products",
@@ -13,7 +14,7 @@ export class ProductListComponent implements OnInit {
   showImage: boolean = false;
 
   private _listFilter: string = "";
-
+  
   get listFilter(): string {
     return this._listFilter;
   }
@@ -24,35 +25,17 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.performFilter(value);
   }
 
-  filteredProducts: Iproduct[] = [];
+  filteredProducts: IProduct[] = [];
 
 
-  products: Iproduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
+  products: IProduct[] = [ ];
 
-  performFilter(filterBy: string): Iproduct[] {
+
+  constructor(private productService: ProductService) { }
+
+  performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: Iproduct) =>
+    return this.products.filter((product: IProduct) =>
       product.productName.toLocaleLowerCase().includes(filterBy));
   }
 
@@ -61,11 +44,12 @@ export class ProductListComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    this.listFilter = "cart";
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   onRatingClicked(message: string): void {
     this.pageTitle = "Product List: " + message;
-   }
+  }
 
 }
